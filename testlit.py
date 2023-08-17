df0ee86981bc8f911d71eedcc449a36af1914959
kdd883 <142468586+kdd883@users.noreply.github.com>
2023-08-17 13:54:47 +1000

    Update testlit.py

--- testlit.py
+++ testlit.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.text('Fixed width text')
+
+"""
 st.markdown('_Markdown_') # see #*
 st.caption('Balloons. Hundreds of them...')
 st.latex(r''' e^{i\pi} + 1 = 0 ''')
@@ -10,3 +12,24 @@ st.title('My title')
 st.header('My header')
 st.subheader('My sub')
 st.code('for i in range(8): foo()')
+
+"""
+
+# Show a spinner during a process
+>>> with st.spinner(text='In progress'):
+>>>   time.sleep(3)
+>>>   st.success('Done')
+
+# Show and update progress bar
+>>> bar = st.progress(50)
+>>> time.sleep(3)
+>>> bar.progress(100)
+
+st.balloons()
+st.snow()
+st.toast('Mr Stay-Puft')
+st.error('Error message')
+st.warning('Warning message')
+st.info('Info message')
+st.success('Success message')
+st.exception(e)
