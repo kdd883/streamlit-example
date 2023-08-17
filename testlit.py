cebcc1dda83a00e1b6678a1d0365e1a5f46c1602
kdd883 <142468586+kdd883@users.noreply.github.com>
2023-08-17 13:56:52 +1000

    Update testlit.py

--- testlit.py
+++ testlit.py
@@ -17,7 +17,6 @@ st.code('for i in range(8): foo()')
 
 # Show a spinner during a process
 with st.spinner(text='In progress'):
-  time.sleep(3)
   st.success('Done')
 
 # Show and update progress bar
